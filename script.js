af28a8d9bbbe0eafa9396e6ea0d5128d0cb3130c
JeongminJamie <sksrhcalsu@gmail.com>
2024-04-26 22:41:29 +0900

    added game over function for when either total score is over than 50

--- script.js
+++ script.js
@@ -1,25 +1,47 @@
-// players
-let currentPlayer;
-let player1 = "player1";
-let player2 = "player2";
+// player
+let currentPlayer = "player1";
+
+// players' scores
+let player1Current = 0;
+let player2Current = 0;
+let player1Total = 0;
+let player2Total = 0;
 
 // players' current scores
-let player1Current = document.querySelector(".game__player1-current").innerHTML;
-let player2Current = document.querySelector(".game__player2-current").innerHTML;
+let player1CurrentElement = document.querySelector(".game__player1-current");
+let player2CurrentElement = document.querySelector(".game__player2-current");
 
 // players' total scores
-let player1Total = document.querySelector(".game__player1-total").innerHTML;
-let player2Total = document.querySelector(".game__player2-total").innerHTML;
+let player1TotalElement = document.querySelector(".game__player1-total");
+let player2TotalElement = document.querySelector(".game__player2-total");
 
+//DOM elements
 const rollBtn = document.querySelector(".game__buttons-roll");
-holdBtn = document.querySelector(".game__buttons-hold");
+const holdBtn = document.querySelector(".game__buttons-hold");
+
+//update the states of the scores
+const updateUI = () => {
+  player1CurrentElement.innerHTML = player1Current;
+  player2CurrentElement.innerHTML = player2Current;
+  player1TotalElement.innerHTML = player1Total;
+  player2TotalElement.innerHTML = player2Total;
+};
+
+// show an alert when either total score is over than 50
+const gameOver = () => {
+  if (player1Total >= 50) {
+    alert("Game Over! Player 1 won the game");
+  } else if (player2Total >= 50) {
+    alert("Game Over! Player 2 won the game");
+  }
+};
 
 //handle dice roll button
 const handleRollBtn = () => {
-  let randomDiceNum = Math.floor(Math.random() * 6);
-  console.log(randomDiceNum);
+  let randomDiceNum = Math.ceil(Math.random() * 6);
+
   //when dice number is 1 or 2
-  if (randomDiceNum === 1 || randomDiceNum === 2) {
+  if (randomDiceNum <= 2) {
     if (currentPlayer === "player1") {
       player1Current = 0;
       currentPlayer = "player2";
@@ -27,16 +49,14 @@ const handleRollBtn = () => {
       player2Current = 0;
       currentPlayer = "player1";
     }
-  }
-
-  //when dice number is over 3
-  if (randomDiceNum >= 3) {
+  } else {
     if (currentPlayer === "player1") {
       player1Current += randomDiceNum;
     } else {
       player2Current += randomDiceNum;
     }
   }
+  updateUI();
 };
 
 //handle hold button
@@ -50,7 +70,14 @@ const handleHoldBtn = () => {
     player2Current = 0;
     currentPlayer = "player1";
   }
+  updateUI();
+
+  //wait for the updateUI and then show the alert
+  setTimeout(() => {
+    gameOver();
+  }, 100);
 };
 
 rollBtn.addEventListener("click", handleRollBtn);
 holdBtn.addEventListener("click", handleHoldBtn);
+updateUI();
